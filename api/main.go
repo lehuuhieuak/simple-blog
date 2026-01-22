e47c7182d8f827d8fd97bc5b53409956981d1153
lehuuhieuak <01643815296h@gmail.com>
2026-01-22 23:07:04 +0700

    feat: add domain to CORS

--- api/main.go
+++ api/main.go
@@ -69,7 +69,7 @@ func main() {
 
 	// CORS middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000", "https://blog.lehuuhieu.dev"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
